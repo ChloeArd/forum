d88820718776934f7c84a52abb3fd527a65957cb
ChloeArd <chloe.ardoise@gmail.com>
2021-10-13 12:00:00 +0200

    Archivé

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
 session_start();
 
-require 'vendor/autoload.php';
+require_once "Model/DB.php";
+require_once "Controller/Traits/ReturnViewTrait.php";
+require_once "Model/Manager/Traits/ManagerTrait.php";
+
+require_once "Controller/CommentController.php";
+require_once "Controller/CategorieController.php";
+require_once "Controller/HomeController.php";
+require_once "Controller/SubjectController.php";
+require_once "Controller/UserController.php";
+
+require_once "Model/Entity/Categorie.php";
+require_once "Model/Entity/Comment.php";
+require_once "Model/Entity/Role.php";
+require_once "Model/Entity/Subject.php";
+require_once "Model/Entity/User.php";
+
+require_once "Model/Manager/CommentManager.php";
+require_once "Model/Manager/CategorieManager.php";
+require_once "Model/Manager/RoleManager.php";
+require_once "Model/Manager/SubjectManager.php";
+require_once "Model/Manager/UserManager.php";
 
 use Chloe\Forum\Controller\CategorieController;
 use Chloe\Forum\Controller\CommentController;
