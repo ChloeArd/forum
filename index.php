75ff4eb9781bae567a372d651f7d7ea3c5116ab0
ChloeArd <chloe.ardoise@gmail.com>
2021-10-15 10:12:00 +0200

    forum composer

--- index.php
+++ index.php
@@ -1,27 +1,7 @@
 <?php
 session_start();
 
-require_once "Model/DB.php";
-require_once "Controller/Traits/ReturnViewTrait.php";
-require_once "Model/Manager/Traits/ManagerTrait.php";
-
-require_once "Controller/CommentController.php";
-require_once "Controller/CategorieController.php";
-require_once "Controller/HomeController.php";
-require_once "Controller/SubjectController.php";
-require_once "Controller/UserController.php";
-
-require_once "Model/Entity/Categorie.php";
-require_once "Model/Entity/Comment.php";
-require_once "Model/Entity/Role.php";
-require_once "Model/Entity/Subject.php";
-require_once "Model/Entity/User.php";
-
-require_once "Model/Manager/CommentManager.php";
-require_once "Model/Manager/CategorieManager.php";
-require_once "Model/Manager/RoleManager.php";
-require_once "Model/Manager/SubjectManager.php";
-require_once "Model/Manager/UserManager.php";
+require 'vendor/autoload.php';
 
 use Forum\Controller\CategorieController;
 use Forum\Controller\CommentController;
